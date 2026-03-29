66adc4bfeee6a5701ffdb7d9b2d494493a652a34
Cheni-10 <esandichenithma@outlook.com>
2026-03-29 14:58:42 +0530

    fix express 5 wildcard

--- backend/src/server.js
+++ backend/src/server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../frontend/build')))
   
   // For any route not matched by API, serve React app
-  app.get('/*', (req, res) => {
+  app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'))
   })
 }
