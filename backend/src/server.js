c5b8b7e2647e1e9b26943a7fe511b3b7cbffb602
Cheni-10 <esandichenithma@outlook.com>
2026-04-17 13:34:25 +0530

    render error fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
     startFRSJob();
     // 3. Initial Recalculation (Non-blocking)
